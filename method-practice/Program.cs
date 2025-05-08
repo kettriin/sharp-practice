d0f7e7cbbdb02c27a24a432f5017bcaec4942433
Katerina Borisova <katerinaborisova@MacBook-Air-Katerina.local>
2025-05-08 13:01:31 +0300

    methods

---
+++ method-practice/Program.cs
@@ -0,0 +1,42 @@
+﻿int[] CreateArray(int n)
+{
+    return new int[n];
+}
+
+void Fill(int[] elements)
+{
+    int len = elements.Length;
+    int index = 0;
+
+    while (index < len)
+    {
+        int current = Random.Shared.Next(30);
+        elements[index] = current;
+        Console.WriteLine(elements[index]);
+        index ++;
+    }
+}
+
+int GetSum(int[] array)
+{
+    int len = array.Length;
+    int sum = 0;
+    int count = 0;
+
+    while (count < len)
+    {
+        int cur = array[count];
+        if (cur >= 10 && cur <= 99)
+        {
+            sum += cur;
+        }
+        count ++;
+    }
+
+    return sum;
+}
+
+
+int[] chisla = CreateArray(10);
+Fill(chisla);
+Console.WriteLine(GetSum(chisla));
