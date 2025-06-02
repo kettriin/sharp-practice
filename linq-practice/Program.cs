65447793160170b74e5150c9ce8aa4d213c94264
Katerina Borisova <katerinaborisova@MacBook-Air-Katerina.local>
2025-06-02 12:50:45 +0300

    linq pract

---
+++ linq-practice/Program.cs
@@ -0,0 +1,8 @@
+﻿
+//linq - язык запросов для фильтрации группировки и тд
+List<int> ints = Enumerable.Range(0, 10)
+    .Select(item => Random.Shared.Next(10))
+    .Where(item => item % 2 == 0)
+    .ToList();
+
+Console.WriteLine(String.Join(", ", ints));
